4f3e698d21ca2f6cc5b94b2cd0568244f90df107
nomarine <rafaelfijos@gmail.com>
2024-01-24 09:16:49 -0300

    Alter node build to watchcommand

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Login (teste componente)</div>
+                    <div class="card-header">Login (teste componente com run watch)</div>
 
                     <div class="card-body">
                         <form method="POST" action="">
