b5afe992e0c80278c77887432bb05d9e618b0881
nomarine <rafaelfijos@gmail.com>
2024-01-26 08:59:59 -0300

    Create form in Marcas pahe; Downgrade Bootstrap version to 4.6

--- resources/js/components/MarcasComponent.vue
+++ resources/js/components/MarcasComponent.vue
@@ -6,7 +6,23 @@
                     <div class="card-header">Marcas</div>
 
                     <div class="card-body">
-                        Manipulação dos registros de marcas.
+                        <div class="form-row">
+                            <div class="col mb-3">
+                                <label for="inputId" class="form-label">ID</label>
+                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp">
+                                <div id="idHelp" class="form-text text-muted">Opcional.</div>
+                            </div>
+
+                            <div class="col mb-3">
+                                <label for="inputNome" class="form-label">Nome da Marca</label>
+                                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp">
+                                <div id="nomeHelp" class="form-text text-muted">Opcional.</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
                     </div>
                 </div>
             </div>
